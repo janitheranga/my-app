fb1b2eb1d929ac2211b388fd58f0eb372074ee6b
Janith Eranga <jeerange@gmail.com>
2023-06-10 22:42:09 +0530

    Updated NFT image URL

--- src/pages/index.js
+++ src/pages/index.js
@@ -38,8 +38,8 @@ export default function Home() {
     // If user is not connected to the Goerli network, let them know and throw an error
     const { chainId } = await web3Provider.getNetwork();
     if (chainId !== 11155111) {
-      window.alert("Change the network to Goerli");
-      throw new Error("Change network to Goerli");
+      window.alert("Change the network to Sepolia");
+      throw new Error("Change network to Sepolia");
     }
 
     if (needSigner) {
@@ -210,7 +210,7 @@ export default function Home() {
           {renderButton()}
         </div>
         <div>
-          <img className={styles.image} src="./crypto-devs.svg" />
+          <img className={styles.image} src="../../public/crypto-devs.svg" />
         </div>
       </div>
 
